ae5db011281e2ded1ed58205b0317eca0342dc92
lily <raichek23@gmail.com>
2022-11-21 00:47:50 +0900

    add Express

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/products', function(req, res) {
+  res.json({'success': true})
+})
+
+const PORT = process.env.PORT || '3001'
+
+app.listen(PORT, function() {
+  console.log('I am running!')
+})
