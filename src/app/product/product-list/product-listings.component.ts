aeba63fb0ee573762d361c59eda0cb3ec85e0847
lily <raichek23@gmail.com>
2022-12-09 00:43:58 +0900

    update product-listings/detail

--- src/app/product/product-list/product-listings.component.ts
+++ src/app/product/product-list/product-listings.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from 'src/app/products';
 
 @Component({
   selector: 'app-product-list',
@@ -8,11 +10,12 @@ import { Component, Input, OnInit } from '@angular/core';
 
 export class ProductListComponent implements OnInit {
 
-  products: any = [1, 2, 3, 4]
+  products: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.products = products
   }
 
 }
